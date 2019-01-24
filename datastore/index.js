fcf46a618b6ee647e7c7d6e292369549ea8b8315
victorx716 <victorliu716@gmail.com>
2019-01-24 12:22:52 -0800

    Implements complete implemenation of basic async functions

--- datastore/index.js
+++ datastore/index.js
@@ -10,11 +10,11 @@ var items = {};
 exports.create = (text, callback) => {
   counter.getNextUniqueId( function(err, id) {
     if (err) {
-      throw ('error, no items')
+      throw ('error, no items');
     } else {
-        fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err) => {
-          if (err) {
-            throw err;
+      fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err) => {
+        if (err) {
+          throw err;
           } else {
               callback(null, { id, text }) 
           }
@@ -24,65 +24,126 @@ exports.create = (text, callback) => {
 };
 
 exports.readAll = (callback) => {
-  // var data = _.map(items, (text, id) => {
-    // return { id, id };
-  // });
-    // callback(null, data);
+
     fs.readdir(exports.dataDir, (err, data) => {
       if(err){
         throw('still not it bruh')
       } else {
-        console.log('data', data);
+        //filter data to just numbers
+      var special = data;
+       for (var i = 0; i< special.length; i++) {
+         special[i] = special[i].replace(/\D+/g, '');
+       }
+        var specialArr = _.map(special, (file) => {
+          return {id: file, text: file};
+        });
+        callback(null, specialArr);
       }
     })
-
-
-
-
-
-
-      // THE RABBIT HOLE THEY TOLD US NOT TO DOOOOOOOOOOO!!!!!!!!
-  // fs.readFile(path.join(exports.dataDir, `${id}.txt`), (err, fileData) => {
-  //   if (err) {
-  //     throw ('error, aint nuffin workin')
-  //   } else {
-  //      let data = _.map(fileData, (text, id) => {
-  //        return { id, text};
-  //      })
-  //   }
-  // });
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
+
+var text;
+fs.readFile(path.join(exports.dataDir, `${id}.txt`), 'utf8', (err, data) => {
+  if (err) {
+    callback(err)
   } else {
-    callback(null, { id, text });
+     text = data;
+     callback(null, { id, text })
   }
+})
+
+
+
+
+
+
+  //-------------------------------------Rabbit hole-------------------------------------
+  // var text = items[id]];
+  // var text;
+  // fs.readdir(exports.dataDir, (err, data) => {
+  //   if (err) {
+  //     throw('nope')
+  //   } else {
+  //      var special = data;
+  //      for (var i = 0; i< special.length; i++) {
+  //        special[i] = special[i].replace(/\D+/g, '');
+  //      }
+
+  //      for (var j = 0; j < special.length; j++) {
+  //        if (special[j] === id) {
+  //          fs.readFile(path.join(exports.dataDir, `${id}.txt`), 'utf8', (err, data) => {
+  //            if (err) {
+  //              throw('try again')
+  //              } else {
+  //               console.log('is data even getting in here?-->', data)
+  //               console.log('is text getting down here? ', text)
+  //               text = data;
+  //              }
+  //           })
+  //        }
+  //     }
+  //     if (!text) {
+  //       callback(new Error(`No item with id: ${id}`));
+  //     } else {
+  //       callback(null, { id, text });
+  //     };
+  //   };
+  // })
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
-};
+  // var item = items[id];
+  // if (!item) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   items[id] = text;
+  //   callback(null, { id, text });
+  // }
+  exports.readOne(id, function(err, data) {
+    if (err) {
+      callback(err);
+    } else {
+        fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err, data) => {
+          if (err) {
+          callback(err);
+      } else {
+          data = text;
+          callback(null, { id, data })
+        }
+      })
+    }
+  })
+
+
+
+}
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
-};
-
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
+  exports.readOne(id, function(err, data) {
+    if (err) {
+      callback(err);
+    } else {
+  fs.unlink(path.join(exports.dataDir, `${id}.txt`), (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log('path/file.txt was deleted');
+      callback();
+    }
+    });
+    }
+});
+}
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
 
 exports.dataDir = path.join(__dirname, 'data');
